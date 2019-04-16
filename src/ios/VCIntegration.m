3e90eb094b70645ef774bd6ac547927a2341647a
Askblue <carlos.costa.cruz@gmail.com>
2019-04-16 11:17:08 +0100

    Update VCIntegration.m

--- src/ios/VCIntegration.m
+++ src/ios/VCIntegration.m
@@ -1,7 +1,7 @@
 #import "VCIntegration.h"
 #import "ViewController.h"
 #import "AskblueUtils.h"
-#import "LoginViewController.h"
+//#import "LoginViewController.h"
 
 @implementation VCIntegration
 
