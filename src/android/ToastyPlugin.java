72f725a25a6e84e9dce52d9bf62f89f80a1d8b8b
Askblue <carlos.costa.cruz@gmail.com>
2019-04-24 22:25:11 +0100

    Update ToastyPlugin.java

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -97,7 +97,7 @@ public class ToastyPlugin extends CordovaPlugin {
           // Intent intent = new Intent(context, MainActivity.class);
         //  that.cordova.getActivity().startActivity(intentScan);
 
-          cordova.setActivityResultCallback (this);
+          that.cordova.setActivityResultCallback (this);
 
           that.cordova.getActivity().startActivityForResult(intentScan);
 
